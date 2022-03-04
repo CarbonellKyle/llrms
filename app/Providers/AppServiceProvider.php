80d02817e7c9dbcda69a9326cf29a800ce881dc9
Kyle Carbonell <s.carbonell.kyle@cmu.edu.ph>
2022-03-04 11:06:19 +0800

    edited AppServiceProvider boot function

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //Change http to https when app_env = production on deployment
+        if($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 }
