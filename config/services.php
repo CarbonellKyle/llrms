a56aff7ab708325c4599cb3fe72775a39217c22e
AdrianPol27 <60411414+AdrianPol27@users.noreply.github.com>
2022-03-07 15:55:55 +0700

    Change to online

--- config/services.php
+++ config/services.php
@@ -17,9 +17,9 @@ return [
     //Google API Credentials
     'google' => [
         //Localhost Google API credentials
-        //'client_id'     => '449502118135-a03i490cc593s03idun47doq2ep9a1ol.apps.googleusercontent.com',
-        //'client_secret' => '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',
-        //'redirect'      => 'http://127.0.0.1:8000/callback/google',
+        // 'client_id'     => '449502118135-a03i490cc593s03idun47doq2ep9a1ol.apps.googleusercontent.com',
+        // 'client_secret' => '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',
+        // 'redirect'      => 'http://127.0.0.1:8000/callback/google',
 
         // Hosted or Production Google API credentials
         'client_id'     => '449502118135-tb8oa3fdrrfi6dkevfc3jfo896mdqkbm.apps.googleusercontent.com',
