36d982858aebfe2e5e14f4c0697f12e4c7bdd899
AdrianPol27 <60411414+AdrianPol27@users.noreply.github.com>
2022-03-07 12:25:14 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <div class="content-wrapper">
-	<section id="form-wrapper" class="shadow-lg px-4">
+	<section id="form-wrapper" class="shadow-lg px-4 bg-white">
 		<div class="text-center">
 			<a href="#"><img src="{{ asset('images/lrmds-logo.png') }}" class="logo img-fluid" alt="LRDMS Logo"></a>
 		</div>
@@ -11,7 +11,7 @@
             @csrf
 
 			<!-- Email Input Start -->
-            <input id="email" type="email" class="my-2 py-2 form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+            <input id="email" type="email" class="my-2 py-2 form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" placeholder="Email" required autocomplete="email" autofocus>
             @error('email')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
@@ -20,7 +20,7 @@
             <!-- Email Input SEnd -->
 			
             <!-- Password Input Start -->
-            <input id="password" type="password" class="my-2 py-2 form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+            <input id="password" type="password" class="my-2 py-2 form-control @error('password') is-invalid @enderror" name="password" placeholder="Password" required autocomplete="current-password">
             @error('password')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
@@ -34,7 +34,7 @@
 				<p class="text-uppercase text-center bg-white px-3">or</p>
 			</div>
 			<a href="{{ url('/redirect') }}"><img src="{{ asset('images/google-signin.png') }}" class="img-fluid" alt="LRDMS Logo"></a>
-			<p class="mt-2">Don't have an account? <a href="signup.php" class="text-decoration-none text-primary">Sign Up Now!</a></p>
+			<p class="mt-2 text-center">Don't have an account? <a href="signup.php" class="text-decoration-none text-primary">Sign Up Now!</a></p>
 		</form>
 	</section>
 </div>
