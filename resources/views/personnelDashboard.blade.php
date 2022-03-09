0870ca275c3703148958b1abc7f6baab192477e1
AdrianPol27 <60411414+AdrianPol27@users.noreply.github.com>
2022-03-09 10:28:42 +0700

    Updated

--- resources/views/personnelDashboard.blade.php
+++ resources/views/personnelDashboard.blade.php
@@ -45,23 +45,4 @@
 			</div>	
 		</div>
 	</div>
-<!-- <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Personnel Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <p>Welcome, <strong>{{ Auth::user()->username }}</strong>! You are Logged in as a <strong>Personnel</strong>!</p>
-                </div>
-            </div>
-        </div>
-    </div>
-</div> -->
 @endsection
--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.personnelLayout')
 
 @section('content')
     <div class="content">
