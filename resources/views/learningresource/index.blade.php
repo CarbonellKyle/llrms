45f9a3dac7264484ecdd384bff7586ad57d3d2e8
AdrianPol27 <60411414+AdrianPol27@users.noreply.github.com>
2022-03-10 12:12:35 +0700

    Update Learning Resource

--- resources/views/learningresource/index.blade.php
+++ resources/views/learningresource/index.blade.php
@@ -8,8 +8,10 @@
             <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
         </svg>
     </button>
-  	<div class="container">
+
+    <div class="col-lg-12">
         <h2 class="text-center">Learning Resource</h2>
         <a href="{{ route('learningresource.upload') }}" class="btn btn-primary mt-4">Upload Files</a>
     </div>
+</div>
 @endsection
