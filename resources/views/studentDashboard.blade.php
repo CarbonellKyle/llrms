39d263bc11d252af367e770906eb695aa3b7d14a
AdrianPol27 <60411414+AdrianPol27@users.noreply.github.com>
2022-03-11 17:02:02 +0700

    Updated

--- resources/views/studentDashboard.blade.php
+++ resources/views/studentDashboard.blade.php
@@ -64,10 +64,10 @@
                                             {{ $file->filedescription }}
                                         </td>
                                         <td class="text-center">
-                                            <a class="btn btn-sm btn-info mt-2" href="/student/resources/previewFile/{{ $file->id }}">Preview File</a>
+                                            <a class="btn btn-sm btn-info mb-1 w-100" href="/student/resources/previewFile/{{ $file->id }}">Preview File</a>
                                             <form method="GET" action="{{ route('download') }}">
                                                 <input type="hidden" name="file_id" value="{{ $file->id }}" >
-                                                <button class="btn btn-sm btn-primary">Download File</button>
+                                                <button class="btn btn-sm btn-primary w-100">Download File</button>
                                             </form>
                                         </td>
                                     </tr>
